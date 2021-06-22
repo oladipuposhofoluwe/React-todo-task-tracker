fc0c911885e5d7ff2e9c64e65b385a969262b029
oladipuposhofoluwe <oladipuposhofoluwe@gmail.com>
2021-06-22 11:11:46 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <f ooter>
+            <p>Copyright &copy 2021</p>
+            <a href='/about'>About</a>
+        </f>
+    )
+}
+
+export default Footer
